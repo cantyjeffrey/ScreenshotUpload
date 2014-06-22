847b69272a945cffd7a84e9fcd6aa99b17b7e687
Jeena <spam@jeenaparadies.net>
2014-06-22 14:13:11 +0200

    Initial Commit

---
+++ ScreenshotUpload/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenshotUpload
+//
+//  Created by Jeena on 2014-06-22.
+//  Copyright (c) 2014 Jabs Nu. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScreenshotUpload/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ScreenshotUpload
+//
+//  Created by Jeena on 2014-06-22.
+//  Copyright (c) 2014 Jabs Nu. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
